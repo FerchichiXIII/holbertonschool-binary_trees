bcb121c47b57d37634d2e43f97dfdcea305b8249
FerchichiXIII <anasferchichi22@gmail.com>
2022-07-20 18:54:18 +0100

    5-binary_tree_is_root.c

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREES_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -29,5 +30,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif
