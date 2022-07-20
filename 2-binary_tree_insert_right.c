fdf78b2685bc2d96780f8287b4d823e28db4419b
FerchichiXIII <anasferchichi22@gmail.com>
2022-07-20 17:49:11 +0100

    2-binary_tree_insert_right.c

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the parent node
+ * @value: value to store in the new node
+ * Return: pointer to the new node, or NULL if failed
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+	if (parent->right != NULL)
+		parent->right->parent = new_node;
+	new_node->right = parent->right;
+	parent->right = new_node;
+	new_node->parent = parent;
+	return (new_node);
+}
