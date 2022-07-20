ef33e0d4e772aa2a19dfb55fb178f83770a5b750
FerchichiXIII <anasferchichi22@gmail.com>
2022-07-20 04:02:17 +0100

    update betty task 1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,8 +1,11 @@
 #include "binary_trees.h"
 /**
- * @brief
- *
+ * binary_tree_node - Creates a new node
+ * @parent: Pointer to the parent node
+ * @value: Value to store in the new node
+ * Return: Pointer to the new node, or NULL if failed
  */
+
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
