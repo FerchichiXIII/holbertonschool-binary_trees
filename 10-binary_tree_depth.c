02588e8564ce142cae04f2f4673891b769183760
FerchichiXIII <anasferchichi22@gmail.com>
2022-07-21 22:47:55 +0100

    10-depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,5 +7,5 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	return ((tree == NULL) ? 0 : 1 + binary_tree_depth(tree->parent));
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0 );
 }
