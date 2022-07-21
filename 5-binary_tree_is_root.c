c52cb4b3d584b09e6dda36fc3b750def3c560d4d
FerchichiXIII <anasferchichi22@gmail.com>
2022-07-21 15:13:04 +0100

    root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,11 +5,11 @@
  * Return: 1 if true, 0 if false
  **/
 
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left == NULL && node->right == NULL)
+	if (node->parent == NULL)
 		return (1);
 	return (0);
 }
